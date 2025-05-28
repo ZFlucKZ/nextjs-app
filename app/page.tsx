f098f89fb95d3996e55bdffe309756833a8c04db
Pajjaphon Whanchid <pajjaphon.whan@gmail.com>
2025-05-28 22:22:10 +0700

    update version

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org Version 2.0.0 →
         </a>
       </footer>
     </div>
